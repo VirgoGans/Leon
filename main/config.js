f439be9bda9525c00cc6f57b5b93edad8e7fb0a7
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2023-04-11 00:56:26 +0530

    Fix bool

--- main/config.js
+++ main/config.js
@@ -7,8 +7,8 @@ module.exports = {
     PREFIX: process.env.PREFIX !== '' ? process.env.PREFIX.charAt(0) : '#',
     LANG: process.env.LANGUAGE !== '' ? process.env.LANGUAGE : 'eng',
     MODE: process.env.MODE !== '' ? process.env.MODE : 'private',
-    AUTOSV: process.env.AUTO_STATUSVIEW !== '' ? process.env.AUTO_STATUSVIEW : Boolean(false),
-    ADMINS: process.env.ADMINS !== '' ? process.env.ADMINS : Boolean(false),
+    AUTOSV: process.env.AUTO_STATUSVIEW !== '' ? process.env.AUTO_STATUSVIEW : 'false',
+    ADMINS: process.env.ADMINS !== '' ? process.env.ADMINS : 'false',
     DATABASE_URL: DATABASE_URL,
     DATABASE: DATABASE_URL === './leon.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: false })
 };
